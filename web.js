c350fee8486ed3576b73416524d4db6b05090020
Andy Macdonald <andy@theandym.com>
2013-07-10 17:33:34 -0400

    modified web.js to read contents of index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var file = fs.readFileSync('index.html');
+
+var buffer = new Buffer(file);
+var content = buffer.toString('utf-8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
