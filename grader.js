834d4c26b075ed5e4a16f96a2a12ac44e4450faa
Andy Macdonald <andy@theandym.com>
2013-07-11 07:48:43 -0400

    fixed issue in grader.js; added checks.json

--- grader.js
+++ grader.js
@@ -29,10 +29,11 @@ var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
   var instr = infile.toString();
-  if(!fs.existsSunc(instr)) {
+  if(!fs.existsSync(instr)) {
     console.log("%s does not exist. Exiting.", instr);
     process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
   }
+  return instr;
 };
 
 var cheerioHtmlFile = function(htmlfile) {
